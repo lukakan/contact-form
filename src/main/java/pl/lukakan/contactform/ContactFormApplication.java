7bf4c80ae3bd7a638bb5a8a8e92d2a336be8c1c4
Lukasz K <kanicki.lukasz@gmail.com>
2021-04-03 12:48:01 +0200

    initial commit

---
+++ src/main/java/pl/lukakan/contactform/ContactFormApplication.java
@@ -0,0 +1,13 @@
+package pl.lukakan.contactform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactFormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lukakan/contactform/ContactFormApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lukakan.contactform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
